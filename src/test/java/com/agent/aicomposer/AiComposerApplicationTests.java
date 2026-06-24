3b353fe6007f3897d74b7250ab7aa5243f3fef25
Yuhao <invulnerableandre@gmail.com>
2026-06-24 12:29:14 +0800

    backend fundational structure

---
+++ src/test/java/com/agent/aicomposer/AiComposerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agent.aicomposer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiComposerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
